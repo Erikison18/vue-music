3343d4b956243c1f7b408228325a1107d9a4b018
Erikson18 <872286926@qq.com>
2018-11-15 17:29:26 +0800

    jsonp

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -8,6 +8,21 @@
 </template>
 
 <script>
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+
 export default{
+  created() {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend() {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res)
+        }
+      })
+    }
+  }
 }
 </script>
