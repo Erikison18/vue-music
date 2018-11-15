3916e448e21cf16baa6bce44d110cd5b13abb0bb
Erik <872286926@qq.com>
2018-11-15 23:29:06 +0800

    slider

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -3,15 +3,34 @@
 
 <template>
     <div class="recommend">
-        recommend
+      <div class="recommend-content">
+        <div class="slider-wrapper" v-if="recommends.length">
+          <slider>
+            <div v-for="(item, index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
+import Slider from 'base/slider/slider'
 
 export default{
+  components: {
+    Slider
+  },
+  data () {
+    return {
+      recommends: []
+    }
+  },
   created() {
     this._getRecommend()
   },
@@ -19,7 +38,7 @@ export default{
     _getRecommend() {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
-          console.log(res)
+          this.recommends = res.data.slider
         }
       })
     }
