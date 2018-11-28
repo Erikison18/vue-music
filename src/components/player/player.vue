02ab2777cbb0b6b94c97fa640f860791a6adba30
Erik <872286926@qq.com>
2018-11-29 07:59:48 +0800

    style player

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -2,6 +2,93 @@
 @import "~common/stylus/variable"
 @import "~common/stylus/mixin"
 
+.player
+  .normal-player
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    z-index 150
+    background $color-background
+    .background
+      position absolute
+      left 0
+      top 0
+      width 100%
+      height 100%
+      z-index -1
+      opacity 0.6
+      filter blur(20px)
+    .top
+      position relative
+      margin-bottom 25px
+      .back 
+        position absolute
+        top 0
+        left 6px
+        z-index 50
+        .icon-back
+          display block
+          padding 9px
+          font-size $font-size-large-x
+          color $color-theme
+          transform rotate(-90deg)
+        .title
+          width 70%
+          margin 0 auto
+          line-height 40px
+          text-align center
+          no-wrap()
+          font-size $font-size-large
+          color $color-text
+        .subtitle
+          line-height 20px
+          text-align center
+          font-size $font-size-medium
+          color $color-text
+    .middle
+      position fixed
+      width 100%
+      top 80px
+      bottom 170px
+      white-space nowrap
+      .middle-l
+        display inline-block
+        vertical-align top
+        position relative
+        width 100%
+        height 0
+        padding-top 80%
+        .cd-wrapper
+          position absolute
+          left 10%
+          top 0
+          width 80px
+          height 100%
+          .cd
+            width 100%
+            height 100%
+            box-sizing border-box
+            border 10px solid rgba(255, 255, 255, 0.1)
+            border-raduis 50%
+            &.play
+              animation rorate 20s linear infinite
+            &.pause
+              animation-play-state paused
+            .image
+              position absolute
+              left 0
+              top 0
+              width 100%
+              height 100%
+              border-radius 50%
+    .bottom
+      position absolute
+      bottom 50px
+      width 100%
+      .dot-wrapper
+  .mini-player
 </style>
 
 <template>
