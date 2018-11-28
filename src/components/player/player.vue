e8609fdd6d89010fcca74464825707d0774b04a2
Erik <872286926@qq.com>
2018-11-28 22:45:06 +0800

    dom player

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,13 +1,65 @@
 <style lang="stylus" scoped>
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
 
 </style>
 
 <template>
   <div class="player" v-show="playList.length>0">
     <div class="normal-player" v-show="fullScreen">
-      播放器
+      <div class="background">
+        <img src="" width="100%" height="100%">
+      </div>
+      <div class="top">
+        <div class="back">
+          <i class="icon-back"></i>
+        </div>
+        <h1 class="title"></h1>
+        <h2 class="subtitle"></h2>
+      </div>
+      <div class="middle">
+        <div class="middle-l">
+          <div class="cd-wrapper">
+            <div class="cd">
+              <img src="" class="image">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <div class="operators">
+          <div class="icon i-left">
+            <i class="icon-sequence"></i>
+          </div>
+          <div class="icon i-left">
+            <i class="icon-prev"></i>
+          </div>
+          <div class="icon i-center">
+            <i class="icon-play"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon-next"></i>
+          </div>
+          <div class="icon i-right">
+            <i class="icon icon-not-favorite"></i>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="mini-player" v-show="!fullScreen">xiao播放器</div>
+    <div class="mini-player" v-show="!fullScreen">
+      <div class="icon">
+        <img src="" width="40" height="40">
+      </div>
+      <div class="text">
+        <h2 class="name"></h2>
+        <p class="desc"></p>
+      </div>
+      <div class="control">
+      </div>
+      <div class="control">
+        <i class="icon-playlist"></i>
+      </div>
+    </div>
   </div>
 </template>
 
